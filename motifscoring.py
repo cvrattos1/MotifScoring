17dad3189ecc5b6ba16ce46edcf6d42e2b22479d
cvrattos1 <cvrattos@princeton.edu>
2020-12-04 21:32:06 -0500

    shifted save

--- motifscoring.py
+++ motifscoring.py
@@ -10,26 +10,39 @@ def scoreMotif(template, result):
 	else:
 		larger = result
 		smaller = template
-	offset = longer.shape[0]
-	length = smaller.shape[0]
-	difference = offset - length
+	longLen = larger.shape[0]
+	shortLen = smaller.shape[0]
+	difference = longLen - shortLen
 
-	for i in range(difference):
-		string1 = larger.loc[offset:(offset+length), :]
-		string2 = smaller.loc[]
-	return test
+	# Loop through all short rightshift alignments
+	for i in range(difference, longLen):
+		string1 = larger
+		smaller.index = range(i,(i+shortLen))
+		string2 = smaller
+		# print(i)
+		# print(string1)
+		# print(string2)
+		score = scoreAlignment(string1, string2)
+		score[0:i] = abs(larger[0:i] - 0.25)
+		score[longLen:longLen+i] = abs(smaller[longLen-i:] - 0.25)
+		# print("SCORE")
+		# print(score)
+		# print(np.sum(score))
+		print(np.sum(score.to_numpy()))
+	return bestScore
 
 def scoreAlignment(template, result):
 	difference = abs(template - result)
+	# result = np.sum(difference)
 	return difference
 
 def main():
 	with open('long_motif/motif.json') as f:
 		template = pd.read_json(f)
-	with open('long_motif/meme.json') as f:
+	with open('long_motif/memeBad.json') as f:
 		meme = pd.read_json(f)
-	print(template)
-	print(meme)
+	# print(template)
+	# print(meme)
 	score = scoreMotif(template, meme)
 	print(score)
 
